17157670f180ee0ba880f229c4cb2a566c2778a3
Jhon Harold Rueda <140807327+Ampexqt@users.noreply.github.com>
2026-08-30 16:09:45 +0800

    Update BoardResolutionTemplate.jsx

--- frontend/src/components/templates/BoardResolutionTemplate.jsx
+++ frontend/src/components/templates/BoardResolutionTemplate.jsx
@@ -63,7 +63,7 @@ export default function BoardResolutionTemplate({
                 padding: "10px",
               }}
             >
-              {document.access_code}
+              {document.access_code || "N/A"}
             </td>
 
             <td

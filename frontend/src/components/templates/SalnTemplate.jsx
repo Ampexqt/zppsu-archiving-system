aa963abf12d93c6c9b159e1add4949693a57e8ae
Jhon Harold Rueda <140807327+Ampexqt@users.noreply.github.com>
2026-09-04 02:00:05 +0800

    Update SalnTemplate.jsx

--- frontend/src/components/templates/SalnTemplate.jsx
+++ frontend/src/components/templates/SalnTemplate.jsx
@@ -28,7 +28,7 @@ export default function SalnTemplate({ data }) {
 
         <tbody>
           <tr>
-            <td style={styles.cell}>{data.access_code}</td>
+            <td style={styles.cell}>{data.access_code || "N/A"}</td>
             <td style={styles.cell}>{data.personnel_name}</td>
             <td style={styles.cell}>{data.year}</td>
             <td style={styles.cell}>{data.file_location}</td>

5eb8736424475c1039f103a97e5f463741507caa
Jhon Harold Rueda <140807327+Ampexqt@users.noreply.github.com>
2026-09-03 21:55:59 +0800

    Update MedicalRecordsTemplate.jsx

--- frontend/src/components/templates/MedicalRecordsTemplate.jsx
+++ frontend/src/components/templates/MedicalRecordsTemplate.jsx
@@ -27,7 +27,7 @@ export default function MedicalRecordsTemplate({ data }) {
 
         <tbody>
           <tr>
-            <td style={styles.cell}>{data.access_code}</td>
+            <td style={styles.cell}>{data.access_code || "N/A"}</td>
             <td style={styles.cell}>{data.name}</td>
             <td style={styles.cell}>{data.file_location}</td>
           </tr>

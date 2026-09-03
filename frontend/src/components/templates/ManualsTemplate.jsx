0734f2c18771556a9487069b3e36a29daec9949a
Jhon Harold Rueda <140807327+Ampexqt@users.noreply.github.com>
2026-09-03 21:55:53 +0800

    Update ManualsTemplate.jsx

--- frontend/src/components/templates/ManualsTemplate.jsx
+++ frontend/src/components/templates/ManualsTemplate.jsx
@@ -27,7 +27,7 @@ export default function ManualsTemplate({ data }) {
 
         <tbody>
           <tr>
-            <td style={styles.cell}>{data.access_code}</td>
+            <td style={styles.cell}>{data.access_code || "N/A"}</td>
             <td style={styles.cell}>{data.subject}</td>
             <td style={styles.cell}>{data.file_location}</td>
           </tr>

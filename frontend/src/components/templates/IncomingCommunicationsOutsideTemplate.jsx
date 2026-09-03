72dcf56795768b209fdf322c532d791927631329
Jhon Harold Rueda <140807327+Ampexqt@users.noreply.github.com>
2026-09-03 21:55:43 +0800

    Update IncomingCommunicationsOutsideTemplate.jsx

--- frontend/src/components/templates/IncomingCommunicationsOutsideTemplate.jsx
+++ frontend/src/components/templates/IncomingCommunicationsOutsideTemplate.jsx
@@ -32,7 +32,7 @@ export default function IncomingCommunicationsOutsideTemplate({ data }) {
         <tbody>
           <tr>
             <td style={styles.cell}>{data.date}</td>
-            <td style={styles.cell}>{data.access_code}</td>
+            <td style={styles.cell}>{data.access_code || "N/A"}</td>
             <td style={styles.cell}>{data.agency}</td>
             <td style={styles.cell}>{data.subject}</td>
             <td style={styles.cell}>{data.action_taken}</td>

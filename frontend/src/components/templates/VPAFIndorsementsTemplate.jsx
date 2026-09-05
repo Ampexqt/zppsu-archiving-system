834857e98b25a6f55fffe4892fd673b8bfa2fc31
Jhon Harold Rueda <140807327+Ampexqt@users.noreply.github.com>
2026-09-06 00:42:49 +0800

    Update VPAFIndorsementsTemplate.jsx

--- frontend/src/components/templates/VPAFIndorsementsTemplate.jsx
+++ frontend/src/components/templates/VPAFIndorsementsTemplate.jsx
@@ -28,7 +28,7 @@ export default function VPAFIndorsementsTemplate({ data }) {
 
         <tbody>
           <tr>
-            <td style={styles.cell}>{data.access_code}</td>
+            <td style={styles.cell}>{data.access_code || "N/A"}</td>
             <td style={styles.cell}>{data.subject}</td>
             <td style={styles.cell}>{data.concerned_personnel}</td>
             <td style={styles.cell}>{data.file_location}</td>
